2372e9b2100c9ebfbaf739c6b01adfc9e6e54b76
vampi62 <vadidi62@gmail.com>
2023-11-15 17:06:39 +0100

    maj mcd avec looping

--- electrostore.sql
+++ electrostore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 192.168.2.52
--- Généré le : mer. 19 juil. 2023 à 20:24
+-- Généré le : mer. 15 nov. 2023 à 15:58
 -- Version du serveur : 10.7.3-MariaDB-1:10.7.3+maria~focal
 -- Version de PHP : 8.0.19
 
@@ -24,30 +24,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `apikeys`
---
-
-CREATE TABLE `apikeys` (
-  `id_apikey` int(11) UNSIGNED NOT NULL,
-  `nom_apikey` varchar(50) NOT NULL,
-  `key_apikey` varchar(200) NOT NULL,
-  `id_user` int(11) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `boxs`
 --
 
 CREATE TABLE `boxs` (
   `id_box` int(11) UNSIGNED NOT NULL,
-  `ymin_box` tinyint(11) UNSIGNED NOT NULL,
-  `xmin_box` tinyint(11) UNSIGNED NOT NULL,
-  `ymax_box` tinyint(11) UNSIGNED NOT NULL,
-  `xmax_box` tinyint(11) UNSIGNED NOT NULL,
-  `id_store` int(11) UNSIGNED NOT NULL,
-  `nbr_compartiment` tinyint(3) UNSIGNED NOT NULL
+  `pos_x_origin_box` tinyint(4) UNSIGNED NOT NULL,
+  `pos_y_origin_box` tinyint(4) UNSIGNED NOT NULL,
+  `taille_x_box` tinyint(4) UNSIGNED NOT NULL,
+  `taille_y_box` tinyint(4) UNSIGNED NOT NULL,
+  `separateur_box` tinyint(4) UNSIGNED NOT NULL,
+  `id_store` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -58,11 +45,11 @@ CREATE TABLE `boxs` (
 
 CREATE TABLE `cameras` (
   `id_camera` int(11) UNSIGNED NOT NULL,
-  `ip_camera` varchar(50) NOT NULL,
-  `port_camera` int(11) UNSIGNED NOT NULL,
-  `flux_camera` varchar(50) NOT NULL,
-  `snapshot_camera` varchar(50) NOT NULL,
-  `nom_camera` varchar(50) NOT NULL
+  `nom_camera` varchar(50) DEFAULT NULL,
+  `ip_camera` varchar(50) DEFAULT NULL,
+  `port_uri_config_camera` varchar(50) DEFAULT NULL,
+  `port_uri_video_camera` varchar(50) DEFAULT NULL,
+  `port_uri_picture_camera` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -73,10 +60,11 @@ CREATE TABLE `cameras` (
 
 CREATE TABLE `commandes` (
   `id_commande` int(11) UNSIGNED NOT NULL,
-  `nom_commande` varchar(50) NOT NULL,
-  `lien_commande` varchar(200) NOT NULL,
   `date_commande` date NOT NULL,
-  `status_commande` varchar(50) NOT NULL
+  `fournisseur_commande` varchar(50) NOT NULL,
+  `status_commande` varchar(50) NOT NULL,
+  `suivi_commandes` varchar(50) DEFAULT NULL,
+  `date_reception_commande` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -86,43 +74,43 @@ CREATE TABLE `commandes` (
 --
 
 CREATE TABLE `commandes_objets` (
-  `id_commande` int(11) UNSIGNED NOT NULL,
   `id_objet` int(11) UNSIGNED NOT NULL,
-  `prix_ttc_U_commandes_objets` float UNSIGNED NOT NULL,
-  `quant_commandes_objets` int(11) UNSIGNED NOT NULL
+  `id_commande` int(11) UNSIGNED NOT NULL,
+  `prix_commandes_objets` decimal(15,2) UNSIGNED NOT NULL,
+  `quant_commandes_objets` tinyint(4) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `droits`
+-- Structure de la table `model_imgs`
 --
 
-CREATE TABLE `droits` (
-  `id_droit` int(11) UNSIGNED NOT NULL,
-  `nom_droit` varchar(50) NOT NULL
+CREATE TABLE `model_imgs` (
+  `id_model_img` int(11) UNSIGNED NOT NULL,
+  `id_model_objet` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `droits_users`
+-- Structure de la table `model_imgs_version_models`
 --
 
-CREATE TABLE `droits_users` (
-  `id_user` int(11) UNSIGNED NOT NULL,
-  `id_droit` int(11) UNSIGNED NOT NULL
+CREATE TABLE `model_imgs_version_models` (
+  `id_model_img` int(11) UNSIGNED NOT NULL,
+  `id_version_model` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `images`
+-- Structure de la table `model_objets`
 --
 
-CREATE TABLE `images` (
-  `id_image` int(11) UNSIGNED NOT NULL,
-  `nom_image` varchar(50) NOT NULL
+CREATE TABLE `model_objets` (
+  `id_model_objet` int(11) UNSIGNED NOT NULL,
+  `nom_model_objet` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -133,15 +121,11 @@ CREATE TABLE `images` (
 
 CREATE TABLE `objets` (
   `id_objet` int(11) UNSIGNED NOT NULL,
-  `nom_objet` varchar(50) NOT NULL,
-  `quant_objet` int(11) UNSIGNED NOT NULL,
-  `description_objet` varchar(50) NOT NULL,
-  `id_box` int(11) UNSIGNED NOT NULL,
-  `min_quant_objet` int(11) UNSIGNED DEFAULT NULL,
-  `lien_achat_objet` varchar(200) DEFAULT NULL,
-  `qrcodeint_objet` varchar(200) NOT NULL,
-  `qrcodeext_objet` varchar(200) DEFAULT NULL,
-  `compartiment_objet` tinyint(3) UNSIGNED NOT NULL
+  `quant_objet` varchar(50) NOT NULL,
+  `seuil_alerte_objet` int(11) UNSIGNED DEFAULT NULL,
+  `max_objet` int(11) UNSIGNED NOT NULL,
+  `description_objet` varchar(300) DEFAULT NULL,
+  `id_box` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -153,9 +137,10 @@ CREATE TABLE `objets` (
 CREATE TABLE `projets` (
   `id_projet` int(11) UNSIGNED NOT NULL,
   `nom_projet` varchar(50) NOT NULL,
-  `description_projet` text NOT NULL,
-  `lien_projet` varchar(200) NOT NULL,
-  `status_projet` varchar(50) NOT NULL
+  `status_projet` varchar(50) NOT NULL,
+  `url_plan_projets` varchar(50) DEFAULT NULL,
+  `date_debut_projet` date NOT NULL,
+  `date_fin_projet` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -165,20 +150,10 @@ CREATE TABLE `projets` (
 --
 
 CREATE TABLE `projets_objets` (
-  `id_projet` int(11) UNSIGNED NOT NULL,
   `id_objet` int(11) UNSIGNED NOT NULL,
-  `quant_projets_objets` int(11) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `projets_users`
---
-
-CREATE TABLE `projets_users` (
   `id_projet` int(11) UNSIGNED NOT NULL,
-  `id_user` int(11) UNSIGNED NOT NULL
+  `quant_projets_objets` tinyint(4) UNSIGNED NOT NULL,
+  `status_projets_objets` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -189,9 +164,10 @@ CREATE TABLE `projets_users` (
 
 CREATE TABLE `stores` (
   `id_store` int(11) UNSIGNED NOT NULL,
-  `nom_store` varchar(50) NOT NULL,
-  `x_store` tinyint(11) UNSIGNED NOT NULL,
-  `y_store` tinyint(11) UNSIGNED NOT NULL
+  `nom_store` varchar(50) DEFAULT NULL,
+  `taille_x_store` tinyint(4) UNSIGNED NOT NULL,
+  `taille_y_store` tinyint(4) UNSIGNED NOT NULL,
+  `sens_store` tinyint(1) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -202,7 +178,8 @@ CREATE TABLE `stores` (
 
 CREATE TABLE `tags` (
   `id_tag` int(11) UNSIGNED NOT NULL,
-  `nom_tag` varchar(50) NOT NULL
+  `nom_tag` varchar(50) NOT NULL,
+  `poid_tag` tinyint(4) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -219,12 +196,12 @@ CREATE TABLE `tags_boxs` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `tags_images`
+-- Structure de la table `tags_model_objets`
 --
 
-CREATE TABLE `tags_images` (
+CREATE TABLE `tags_model_objets` (
   `id_tag` int(11) UNSIGNED NOT NULL,
-  `id_image` int(11) UNSIGNED NOT NULL
+  `id_model_objet` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -241,13 +218,26 @@ CREATE TABLE `tags_objets` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Structure de la table `utilisateurs`
 --
 
-CREATE TABLE `users` (
-  `id_user` int(11) UNSIGNED NOT NULL,
-  `pseudo_user` varchar(50) NOT NULL,
-  `mdp_user` varchar(200) NOT NULL
+CREATE TABLE `utilisateurs` (
+  `id_utilisateur` int(11) UNSIGNED NOT NULL,
+  `pseudo_utilisateur` varchar(50) NOT NULL,
+  `mdp_utilisateur` varchar(250) NOT NULL,
+  `id_version_model` int(11) UNSIGNED DEFAULT NULL,
+  `id_camera` int(11) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `version_models`
+--
+
+CREATE TABLE `version_models` (
+  `id_version_model` int(11) UNSIGNED NOT NULL,
+  `date_version_model` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -255,19 +245,11 @@ CREATE TABLE `users` (
 --
 
 --
--- Index pour la table `apikeys`
---
-ALTER TABLE `apikeys`
-  ADD PRIMARY KEY (`id_apikey`),
-  ADD UNIQUE KEY `key_apikey` (`key_apikey`),
-  ADD KEY `id_user` (`id_user`);
-
---
 -- Index pour la table `boxs`
 --
 ALTER TABLE `boxs`
   ADD PRIMARY KEY (`id_box`),
-  ADD KEY `id_store` (`id_store`);
+  ADD KEY `boxs_ibfk_1` (`id_store`);
 
 --
 -- Index pour la table `cameras`
@@ -285,34 +267,35 @@ ALTER TABLE `commandes`
 -- Index pour la table `commandes_objets`
 --
 ALTER TABLE `commandes_objets`
-  ADD PRIMARY KEY (`id_commande`,`id_objet`),
-  ADD KEY `commandes_objets_ibfk_2` (`id_objet`);
+  ADD PRIMARY KEY (`id_objet`,`id_commande`),
+  ADD KEY `commandes_objets_ibfk_2` (`id_commande`);
 
 --
--- Index pour la table `droits`
+-- Index pour la table `model_imgs`
 --
-ALTER TABLE `droits`
-  ADD PRIMARY KEY (`id_droit`);
+ALTER TABLE `model_imgs`
+  ADD PRIMARY KEY (`id_model_img`),
+  ADD KEY `model_imgs_ibfk_1` (`id_model_objet`);
 
 --
--- Index pour la table `droits_users`
+-- Index pour la table `model_imgs_version_models`
 --
-ALTER TABLE `droits_users`
-  ADD PRIMARY KEY (`id_user`,`id_droit`),
-  ADD KEY `droits_users_ibfk_2` (`id_droit`);
+ALTER TABLE `model_imgs_version_models`
+  ADD PRIMARY KEY (`id_model_img`,`id_version_model`),
+  ADD KEY `model_imgs_version_models_ibfk_2` (`id_version_model`);
 
 --
--- Index pour la table `images`
+-- Index pour la table `model_objets`
 --
-ALTER TABLE `images`
-  ADD PRIMARY KEY (`id_image`);
+ALTER TABLE `model_objets`
+  ADD PRIMARY KEY (`id_model_objet`);
 
 --
 -- Index pour la table `objets`
 --
 ALTER TABLE `objets`
   ADD PRIMARY KEY (`id_objet`),
-  ADD KEY `id_box` (`id_box`);
+  ADD KEY `objets_ibfk_1` (`id_box`);
 
 --
 -- Index pour la table `projets`
@@ -324,15 +307,8 @@ ALTER TABLE `projets`
 -- Index pour la table `projets_objets`
 --
 ALTER TABLE `projets_objets`
-  ADD PRIMARY KEY (`id_projet`,`id_objet`),
-  ADD KEY `projet_objets_ibfk_2` (`id_objet`);
-
---
--- Index pour la table `projets_users`
---
-ALTER TABLE `projets_users`
-  ADD PRIMARY KEY (`id_projet`,`id_user`),
-  ADD KEY `projets_users_ibfk_2` (`id_user`);
+  ADD PRIMARY KEY (`id_objet`,`id_projet`),
+  ADD KEY `projets_objets_ibfk_2` (`id_projet`);
 
 --
 -- Index pour la table `stores`
@@ -351,37 +327,39 @@ ALTER TABLE `tags`
 --
 ALTER TABLE `tags_boxs`
   ADD PRIMARY KEY (`id_box`,`id_tag`),
-  ADD KEY `id_tag` (`id_tag`);
+  ADD KEY `tags_boxs_ibfk_2` (`id_tag`);
 
 --
--- Index pour la table `tags_images`
+-- Index pour la table `tags_model_objets`
 --
-ALTER TABLE `tags_images`
-  ADD PRIMARY KEY (`id_tag`,`id_image`),
-  ADD KEY `id_image` (`id_image`);
+ALTER TABLE `tags_model_objets`
+  ADD PRIMARY KEY (`id_tag`,`id_model_objet`),
+  ADD KEY `tags_model_objets_ibfk_2` (`id_model_objet`);
 
 --
 -- Index pour la table `tags_objets`
 --
 ALTER TABLE `tags_objets`
   ADD PRIMARY KEY (`id_objet`,`id_tag`),
-  ADD KEY `id_tag` (`id_tag`);
+  ADD KEY `tags_objets_ibfk_2` (`id_tag`);
 
 --
--- Index pour la table `users`
+-- Index pour la table `utilisateurs`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id_user`);
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id_utilisateur`),
+  ADD KEY `utilisateurs_ibfk_1` (`id_version_model`),
+  ADD KEY `utilisateurs_ibfk_2` (`id_camera`);
+
+--
+-- Index pour la table `version_models`
+--
+ALTER TABLE `version_models`
+  ADD PRIMARY KEY (`id_version_model`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
-
---
--- AUTO_INCREMENT pour la table `apikeys`
---
-ALTER TABLE `apikeys`
-  MODIFY `id_apikey` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `boxs`
@@ -402,16 +380,16 @@ ALTER TABLE `commandes`
   MODIFY `id_commande` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table `droits`
+-- AUTO_INCREMENT pour la table `model_imgs`
 --
-ALTER TABLE `droits`
-  MODIFY `id_droit` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `model_imgs`
+  MODIFY `id_model_img` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table `images`
+-- AUTO_INCREMENT pour la table `model_objets`
 --
-ALTER TABLE `images`
-  MODIFY `id_image` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `model_objets`
+  MODIFY `id_model_objet` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `objets`
@@ -438,20 +416,20 @@ ALTER TABLE `tags`
   MODIFY `id_tag` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table `users`
+-- AUTO_INCREMENT pour la table `utilisateurs`
 --
-ALTER TABLE `users`
-  MODIFY `id_user` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `utilisateurs`
+  MODIFY `id_utilisateur` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `version_models`
+--
+ALTER TABLE `version_models`
+  MODIFY `id_version_model` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- Contraintes pour les tables déchargées
 --
-
---
--- Contraintes pour la table `apikeys`
---
-ALTER TABLE `apikeys`
-  ADD CONSTRAINT `apikeys_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `boxs`
@@ -463,15 +441,21 @@ ALTER TABLE `boxs`
 -- Contraintes pour la table `commandes_objets`
 --
 ALTER TABLE `commandes_objets`
-  ADD CONSTRAINT `commandes_objets_ibfk_1` FOREIGN KEY (`id_commande`) REFERENCES `commandes` (`id_commande`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `commandes_objets_ibfk_2` FOREIGN KEY (`id_objet`) REFERENCES `objets` (`id_objet`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `commandes_objets_ibfk_1` FOREIGN KEY (`id_objet`) REFERENCES `objets` (`id_objet`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `commandes_objets_ibfk_2` FOREIGN KEY (`id_commande`) REFERENCES `commandes` (`id_commande`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `droits_users`
+-- Contraintes pour la table `model_imgs`
 --
-ALTER TABLE `droits_users`
-  ADD CONSTRAINT `droits_users_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `droits_users_ibfk_2` FOREIGN KEY (`id_droit`) REFERENCES `droits` (`id_droit`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `model_imgs`
+  ADD CONSTRAINT `model_imgs_ibfk_1` FOREIGN KEY (`id_model_objet`) REFERENCES `model_objets` (`id_model_objet`) ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `model_imgs_version_models`
+--
+ALTER TABLE `model_imgs_version_models`
+  ADD CONSTRAINT `model_imgs_version_models_ibfk_1` FOREIGN KEY (`id_model_img`) REFERENCES `model_imgs` (`id_model_img`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `model_imgs_version_models_ibfk_2` FOREIGN KEY (`id_version_model`) REFERENCES `version_models` (`id_version_model`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `objets`
@@ -483,15 +467,8 @@ ALTER TABLE `objets`
 -- Contraintes pour la table `projets_objets`
 --
 ALTER TABLE `projets_objets`
-  ADD CONSTRAINT `projet_objets_ibfk_1` FOREIGN KEY (`id_projet`) REFERENCES `projets` (`id_projet`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `projet_objets_ibfk_2` FOREIGN KEY (`id_objet`) REFERENCES `objets` (`id_objet`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `projets_users`
---
-ALTER TABLE `projets_users`
-  ADD CONSTRAINT `projets_users_ibfk_1` FOREIGN KEY (`id_projet`) REFERENCES `projets` (`id_projet`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `projets_users_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `projets_objets_ibfk_1` FOREIGN KEY (`id_objet`) REFERENCES `objets` (`id_objet`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `projets_objets_ibfk_2` FOREIGN KEY (`id_projet`) REFERENCES `projets` (`id_projet`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `tags_boxs`
@@ -501,11 +478,11 @@ ALTER TABLE `tags_boxs`
   ADD CONSTRAINT `tags_boxs_ibfk_2` FOREIGN KEY (`id_tag`) REFERENCES `tags` (`id_tag`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `tags_images`
+-- Contraintes pour la table `tags_model_objets`
 --
-ALTER TABLE `tags_images`
-  ADD CONSTRAINT `tags_images_ibfk_1` FOREIGN KEY (`id_tag`) REFERENCES `tags` (`id_tag`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `tags_images_ibfk_2` FOREIGN KEY (`id_image`) REFERENCES `images` (`id_image`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `tags_model_objets`
+  ADD CONSTRAINT `tags_model_objets_ibfk_1` FOREIGN KEY (`id_tag`) REFERENCES `tags` (`id_tag`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tags_model_objets_ibfk_2` FOREIGN KEY (`id_model_objet`) REFERENCES `model_objets` (`id_model_objet`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `tags_objets`
@@ -513,6 +490,13 @@ ALTER TABLE `tags_images`
 ALTER TABLE `tags_objets`
   ADD CONSTRAINT `tags_objets_ibfk_1` FOREIGN KEY (`id_objet`) REFERENCES `objets` (`id_objet`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `tags_objets_ibfk_2` FOREIGN KEY (`id_tag`) REFERENCES `tags` (`id_tag`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD CONSTRAINT `utilisateurs_ibfk_1` FOREIGN KEY (`id_version_model`) REFERENCES `version_models` (`id_version_model`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `utilisateurs_ibfk_2` FOREIGN KEY (`id_camera`) REFERENCES `cameras` (`id_camera`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
