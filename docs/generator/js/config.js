3dec987fcdcc2d0735c72fad5af23e422165a132
vampi62 <104321401+vampi62@users.noreply.github.com>
2026-02-08 10:16:03 +0100

    Refactor Vault config, generator and API fixes (#54)

--- docs/generator/js/config.js
+++ docs/generator/js/config.js
@@ -176,7 +176,7 @@ function collectConfig(formData) {
 
 // Generate random password
 function generateRandomPassword(length) {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*_+-|;,.<>?';
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#%^&*_+-|;,.<>?';
     let password = '';
     for (let i = 0; i < length; i++) {
         password += chars.charAt(Math.floor(Math.random() * chars.length));
