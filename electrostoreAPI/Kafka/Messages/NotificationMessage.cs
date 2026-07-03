386b311b7bb4166c7664b4122b53fcc65c5175a7
vampi62 <104321401+vampi62@users.noreply.github.com>
2026-07-03 21:12:58 +0200

    Update NotificationMessage.cs

--- electrostoreAPI/Kafka/Messages/NotificationMessage.cs
+++ electrostoreAPI/Kafka/Messages/NotificationMessage.cs
@@ -5,8 +5,11 @@ public class NotificationMessage
     public List<string> Types { get; set; } = ["email"];
     public string? RecipientEmail { get; set; }
     public int? RecipientUserId { get; set; }
-    public string Subject { get; set; } = string.Empty;
-    public string Title { get; set; } = string.Empty;
-    public string Body { get; set; } = string.Empty;
+    public string? TemplateId { get; set; }
+    public string? Language { get; set; }
+    public Dictionary<string, string>? TemplateValues { get; set; }
+    public string? Subject { get; set; }
+    public string? Title { get; set; }
+    public string? Body { get; set; }
     public Dictionary<string, string>? PushData { get; set; }
 }
